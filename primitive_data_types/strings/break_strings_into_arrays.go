d32805faf29c4e5f60feb22e87876485e31e9c3e
Cisgo Scottie M. Klemt <rest@cisgo.io>
2023-01-25 15:17:30 +0100

    20230125

--- primitive_data_types/strings/break_strings_into_arrays.go
+++ primitive_data_types/strings/break_strings_into_arrays.go
@@ -1,19 +1,38 @@
 //ref: https://stackoverflow.com/questions/73017827/golang-convert-string-in-to-an-array
 
+package main
+
 import(
  "fmt" 
+ "strings"
 )
 
-fun trim_and_split(my_string)string{
+func trim_and_split__eg01(my_string string)[]string{
   
   trimmedStr := strings.Trim("[\"firsName\",\"lastName\", \"email\"]", "[]")
-  fmt.Println(strings.Split(trimmedStr, ","))
+
+  //res := strings.Split(trimmedStr, ",")
+  //fmt.Println()
+  return strings.Split(trimmedStr, ",")
+  
+}
+
+func trim_and_split__eg02(my_string string)[]string{
+  
+  //trimmedStr := strings.Trim(my_string)
+
+  //res := strings.Split(trimmedStr, ",")
+  //fmt.Println()
+  return strings.Split(my_string, "'")
   
 }
 
 
 func main(){
   
-  fmt.Println(trim_and_split("mystring'device"))
+  fmt.Println(trim_and_split__eg01("mystring'device"))
 
+  res_eg01 :=trim_and_split__eg02("mystring'device")
+  fmt.Println(trim_and_split__eg02("mystring'device"))
+  fmt.Println(res_eg01[1])
 }
